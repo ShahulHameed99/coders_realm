4ec6201abc898a9cdba6f462e5079dcaea6bbc9c
Shahul Hameed <ShahulHameed.sm@dsrc.in>
2023-08-21 16:48:54 +0530

    Transform code added

--- Transform/src/Transform/Transform.js
+++ Transform/src/Transform/Transform.js
@@ -2,8 +2,21 @@ import './Transform.css'
 
 function Transform() {
     return (
-        <div>
-            Hi
+        <div class='transform'>
+            <div class='box1' />
+            <p>Scale</p>
+            <div class='box2' />
+            <p>ScaleX</p>
+            <div class='box3' />
+            <p>ScaleY</p>
+            <div class='box4' />
+            <p>Rotate</p>
+            <div class='box5' />
+            <p>Skew</p>
+            <div class='box6' />
+            <p>Translate</p>
+            <div class='box7' />
+            <p>Matrix</p>
         </div>
     )
 }
