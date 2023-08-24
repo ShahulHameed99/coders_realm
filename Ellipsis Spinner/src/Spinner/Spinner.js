da015f5f1c55659485e512c413b12b299f7260fc
Shahul Hameed <ShahulHameed.sm@dsrc.in>
2023-08-24 10:29:52 +0530

    Ellipsis Spinner

---
+++ Ellipsis Spinner/src/Spinner/Spinner.js
@@ -0,0 +1,17 @@
+import './Spinner.css'
+
+function Spinner() {
+    return (
+        <div>
+            <div class='lds-ellipsis'>
+                <div />
+                <div />
+                <div />
+                <div />
+            </div>
+        </div>
+
+    )
+}
+
+export default Spinner
